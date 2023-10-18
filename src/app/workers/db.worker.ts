e4f49354dc6677dea9e60494f69ec53538c4d7eb
Rebolon <richard.tribes@gmail.com>
2023-10-18 08:09:44 +0200

    comment worker : maybe remove interface usage and just insert data for instance

---
+++ src/app/workers/db.worker.ts
@@ -0,0 +1,38 @@
+//import { HttpClient, HttpHeaders, HttpXhrBackend } from '@angular/common/http';
+//import { Observable, from, map, switchMap, toArray } from 'rxjs';
+
+
+addEventListener('message', ({ data }) => {
+  postMessage(`worker response to ${data}`);
+});
+/*import { Lang, Word, db } from '../services/database/db';
+
+addEventListener('message', ({ data }) => {
+  populate().subscribe((promiseResult) => {
+    console.log('insert result', promiseResult);
+    postMessage(`worker response to ${data}`);
+  })
+});
+
+function populate(): Observable<unknown> {
+  const httpClient = new HttpClient(new HttpXhrBackend({ 
+    build: () => new XMLHttpRequest() 
+  }));
+  
+  return httpClient.get('/assets/dictionnaries/fr/ods6.txt', {
+    headers: new HttpHeaders({
+       'Accept':'text'
+    }),
+    'responseType': 'text'
+  }).pipe(
+    switchMap(dic => from(dic.split(/\n/))),
+    map(word => {
+      return {
+        lang: Lang.FR,
+        value: word
+      } as Word
+    }),
+    toArray(),
+    switchMap((words) => from(db.words.bulkAdd(words)))
+  )
+}*/
